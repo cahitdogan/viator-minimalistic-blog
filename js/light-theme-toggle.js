12101a4c66d8c617b0a52a1bd0a5217ea13d3109
Cahit <103262027+cahitdogan@users.noreply.github.com>
2023-08-02 20:59:36 +0300

    Add files via upload

---
+++ js/light-theme-toggle.js
@@ -0,0 +1,22 @@
+let currentTheme = localStorage.getItem("theme") || "dark";
+document.addEventListener("DOMContentLoaded", function () {
+    const bodyElement = document.body;
+    bodyElement.classList.add("theme-transition");
+});
+
+function pickTheme() {
+    switch (currentTheme) {
+        case "dark":
+            root.classList.remove("dark");
+            root.classList.add("light");
+            localStorage.setItem("theme", "light");
+            currentTheme = "light";
+            break;
+
+        case "light":
+            root.classList.remove("light");
+            root.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+            currentTheme = "dark";
+    }
+}
